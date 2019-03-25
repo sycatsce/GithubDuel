a5e72c34ef5a92841f439a698308f57eff6aaec4
BRUN.Emmanuel <ebrun@cegos.fr>
2019-03-25 16:14:25 +0100

    loading

--- src/features/Popular/PopularScreen.js
+++ src/features/Popular/PopularScreen.js
@@ -1,37 +1,47 @@
 import React from 'react';
-import { Text, View, TouchableHighlight, FlatList } from 'react-native';
+import { Text, View, TouchableHighlight, FlatList, ActivityIndicator } from 'react-native';
 import * as api from '../../api/github';
 
 export default class PopularScreen extends React.Component{
 
   constructor(props){
     super(props);
-    this.state = { repositories : [] };
+    this.state = { repositories : [], loading: false, error: false };
   }
 
   componentDidMount(){
-    api.getPopularRepos().then( (repositories) => { this.setState({ repositories: repositories.items }); console.log(this.state.repositories); });
+    this.setState({ loading: true });
+    api.getPopularRepos()
+      .then( (repositories) => { this.setState({ repositories: repositories.items, loading: false}) })
+      .catch( (error) => this.setState({ error: true }) );
   }
 
   render() {
+    if (this.state.error == true){ return ( <View><Text> Could not display repositories </Text></View> ) }
     return(
         <View>
-            <View>
-                <Text> Popular Repositories </Text>
-            </View>
+            {
+              this.state.loading == true ?
+              <ActivityIndicator size="large" /> :
+              <View> 
+                <View>
+                    <Text> Popular Repositories </Text>
+                </View>
 
-            <View>
-              <FlatList
-                data={this.state.repositories}
-                renderItem={ (repo) => (
-                    <View>
-                      <Text style={{ fontWeight: 'bold' }}>{ repo.item.name } </Text>
-                      <Text style={{ fontSize: 15 }}>{ repo.item.stargazers_count + "\n" }</Text>
-                    </View>
-                )}
-              />
-            </View>
-        </View>
+                <View>
+                  <FlatList
+                    data={this.state.repositories}
+                    renderItem={ (repo) => (
+                        <View>
+                          <Text style={{ fontWeight: 'bold' }}>{ repo.item.name } </Text>
+                          <Text style={{ fontSize: 15 }}>{ repo.item.stargazers_count + "\n" }</Text>
+                        </View>
+                    )}
+                  /> 
+                </View>
+              </View>
+            }
+        </View> 
     );
   }
 }
